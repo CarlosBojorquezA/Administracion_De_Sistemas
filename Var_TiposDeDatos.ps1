6ac28261a75d4fbddd3a6636d76631fd3fcad92e
CarlosBojorquezA <carlosbojorquez1326@gmail.com>
2025-02-06 00:15:01 -0800

    Create Var_TiposDeDatos.ps1

---
+++ Var_TiposDeDatos.ps1
@@ -0,0 +1,19 @@
+$variable1 = "hola "
+$variable2 = "mundo"
+
+$variable1 + $variable2
+
+$variable3 = 800
+$variable4 = 600
+
+$variable3 + $variable4
+
+$variable3 - $variable4
+
+$variable3 * $variable4
+
+$variable3 / $variable4
+
+$variable3 % $variable4
+
+$variable1.GetType()
